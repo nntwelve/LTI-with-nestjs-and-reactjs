3e2c57450e604cc7c26d66d6c1f9023757fdbde1
Twelve <ntngoc@drimaes.com>
2023-02-06 13:11:47 +0700

    test/dev

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const shortid = require('shortid');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config_service = app.get(ConfigService);
   const port = config_service.get<string>('PORT');
+  console.log(shortid.generate());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
